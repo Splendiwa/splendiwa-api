522585cb5735f38c4e9e3eef30a77b6999c5172a
Nattharika <Nattharika25kata@gmail.com>
2024-11-29 22:30:35 +0700

    Init typescript

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import express, { Express, Request, Response } from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -12,23 +12,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const uri: string =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/your-app";
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/your-app';
 
 (async () => {
-  try {
-    await mongoose.connect(uri);
-    console.log("Connected to the database");
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to the database');
+    } catch(error) {
+        console.error(error);
+    }
 })();
 
-app.get("/health", (_req: Request, res: Response) => {
-  res.status(200).send("Server is running");
+app.get('/health', (_req: Request, res: Response) => {
+    res.status(200).send('Server is running');
 });
 
-const PORT: string | number = process.env.PORT || 3000;
+const PORT: string | number = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
+    console.log(`Server is running on PORT: ${PORT}`);
 });
